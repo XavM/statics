0917d809459ce4884539f07cda489059faf6eb00
XavM <mxav@yahoo.com>
2024-11-05 17:48:03 +0100

    Create devOpsLibsNewWindow.js

---
+++ js/devOpsLibsNewWindow.js
@@ -0,0 +1,18 @@
+const baseURL = window.location.origin + window.location.pathname.split('/').slice(0,3).join('/')
+
+const variableGroups = await (await fetch(`${baseURL}/_apis/distributedtask/variablegroups?continuationToken=0&queryOrder=0`, {
+  "headers": {
+    "accept": "application/json;api-version=7.2-preview.2;excludeUrls=true"
+  }
+})).json()
+
+document.querySelectorAll('.lib-item-var-name').forEach(i => {
+  i.addEventListener('click', (e) => {
+    e.stopImmediatePropagation()
+    const id = variableGroups.value.filter(j => {
+      return j.name == i.innerText
+    }).map(k => k.id)[0]
+    console.log(i.innerText, id);
+    window.open('https://dev.azure.com/ddslogistics2/Saas_Factory/_library?view=VariableGroupView&variableGroupId=19')
+  })
+})
